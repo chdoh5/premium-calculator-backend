195c77a8ede100d0f3e4f2c05722b5b326fe26e5
github username <github email address>
2020-05-27 15:19:23 -0500

    readme

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_04_01_181518) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "results", force: :cascade do |t|
     t.boolean "emailed_efi", default: false
     t.string "generated_at"
